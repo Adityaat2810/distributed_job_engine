94ff0c7162c383012a4a1d8ada385350e80e459f
Aditya <adityasainics10@gmail.com>
2025-09-12 16:01:18 +0530

    [cleanup] commit hook

--- apps/jobber-auth/src/main.ts
+++ apps/jobber-auth/src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true })) // perform validation on dto  
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true })); // perform validation on dto
   const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(

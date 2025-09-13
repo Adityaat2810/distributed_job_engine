e28642ca8622371e3fb8f4b642acd11331f88264
Aditya <adityasainics10@gmail.com>
2025-09-13 14:52:34 +0530

    [cleanup] done

--- apps/jobber-auth/src/main.ts
+++ apps/jobber-auth/src/main.ts
@@ -7,13 +7,14 @@ import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true })); // perform validation on dto
-  const port = process.env.PORT || 3000;
+  const port = app.get(ConfigService).getOrThrow('AUTH_PORT');
   await app.listen(port);
   Logger.log(
     `🚀 Applic ation is running on: http://localhost:${port}/${globalPrefix}`
